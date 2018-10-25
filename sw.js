de97c28e77c7fb05e0a88455f5ca93fb7a05dd94
Samuel Ramirez <samuel_ramirez@live.com.mx>
2018-10-25 17:17:16 -0500

    PRIMER COMMIT

---
+++ sw.js
@@ -0,0 +1,75 @@
+// imports
+importScripts('js/sw-utils.js');
+
+const STATIC_CACHE = 'static-v2';
+const DYNAMIC_CACHE = 'dynamic-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
+
+// LO QUE SE TIENE QUE CARGAR CUANTO ANTES
+const APP_SHELL = [
+  '/',
+  'index.html',
+  'css/style.css',
+  'img/favicon.ico',
+  'img/avatars/spiderman.jpg',
+  'img/avatars/ironman.jpg',
+  'img/avatars/hulk.jpg',
+  'img/avatars/wolverine.jpg',
+  'js/app.js',
+  'js/sw-utils.js'
+];
+
+// LO QUE NUNCA SE MODIFICA
+const APP_SHELL_INMUTABLE = [
+  'https://fonts.googleapis.com/css?family=Quicksand:300,400',
+  'https://fonts.googleapis.com/css?family=Lato:400,300',
+  'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+  'css/animate.css',
+  'js/libs/jquery.js'
+];
+
+
+// Instalación del SW
+self.addEventListener('install', e => {
+
+  const cacheStatic = caches.open( STATIC_CACHE ).then( cache => cache.addAll( APP_SHELL ));
+  const cacheInmutable = caches.open( INMUTABLE_CACHE ).then( cache => cache.addAll( APP_SHELL_INMUTABLE ));
+
+  e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ]) );
+});
+
+// Activación del SW
+self.addEventListener('activate', e => {
+
+  const respuesta = caches.keys().then( keys =>{
+    keys.forEach( key => {
+      if ( key !== STATIC_CACHE && key.includes('static') ) {
+        return caches.delete( key );
+      }
+    });
+  });
+
+  e.waitUntil( respuesta );
+
+});
+
+
+self.addEventListener('fetch', e => {
+
+  const respuesta = caches.match( e.request ).then( res => {
+    if (res) {
+      return res;
+    } else{
+
+      return fetch(e.request).then( newRes => {
+
+        return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes );
+
+      });
+
+    }
+  });
+
+  e.waitUntil( respuesta );
+
+});
